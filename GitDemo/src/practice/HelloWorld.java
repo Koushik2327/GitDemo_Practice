acf03db03fa44810556ef3de9af7beba29f17666
Koushik2327 <kgoud7340@gmail.com>
2026-09-05 09:59:03 +0530

    added new line

--- GitDemo/src/practice/HelloWorld.java
+++ GitDemo/src/practice/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	//its main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("New World");
 		System.out.println("Hello");
 	}
 
